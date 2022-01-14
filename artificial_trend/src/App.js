a8e48c307c26ccdc48f09a1b309888e54562cef2
tatsuomi <34530391+tatsuomi@users.noreply.github.com>
2022-01-15 04:03:07 +0900

    make checkbox

--- artificial_trend/src/App.js
+++ artificial_trend/src/App.js
@@ -6,13 +6,13 @@ class ArtificialTrend extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      cities: this.get_cities(),
+      cities: [],
       city_flags: Array(47).fill(false)
     };
   }
 
   //都道府県取得
-  get_cities(){
+  componentDidMount(){
     //仮の都道府県配列
     var tmp_cities = [];
     fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures/", {
@@ -21,19 +21,50 @@ class ArtificialTrend extends Component{
     .then(response => response.json())
     .then(json => {
       for(var i=0; i<json.result.length; i++){
-        tmp_cities.push(json.result[i].prefName);
+        tmp_cities.push(json.result[i].prefName)
       }
+      this.setState({cities: tmp_cities}, ()=>{
+        console.debug(this.state.cities)
+      })
     });
-    return tmp_cities;
   }
 
   get_API_KEY(){
+    
+  }
+
+  //チェックボックスの雛形
+  check_box(id, city){
+    console.debug(id);
+    return(
+      <div class="checkbox">
+        <input 
+          type="checkbox"
+          checked={this.state.city_flags[id]}
+          onChange={this.toggle()}
+        />
+          {city}
+      </div>
+    );
+  }
+
+  //on/off反転
+  toggle(){
 
   }
 
   //表示
   render(){
-
+    var list = this.state.cities;
+    
+    return(
+      <div>
+        <div>都道府県</div>
+        <div class="checkbox_wrapper">
+          {list.map((city, i) => (this.check_box(i, city)))}
+        </div>
+      </div>
+    )
   }
 }
 
