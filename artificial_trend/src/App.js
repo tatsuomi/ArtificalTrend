6c60e4ca8563d151fbec5036976dbc185d220fae
tatsuomi <34530391+tatsuomi@users.noreply.github.com>
2022-01-14 23:16:02 +0900

    make get_cities

--- artificial_trend/src/App.js
+++ artificial_trend/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ArtificialTrend extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      API_KEY: "",
+      cities: [],
+      city_flags: Array(47).fill(false)
+    };
+    this.get_API_KEY();
+    this.get_cities();
+  }
+
+  //都道府県取得
+  get_cities(){
+    //仮の都道府県配列
+    var tmp_cities = [];
+    fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures/", {
+      headers: {"X-API-KEY": this.state.API_KEY}
+    })
+    .then(response => response.json())
+    .then(json => {
+      for(var i=0; i<json.result.length; i++){
+        tmp_cities.push(json.result[i].prefName);
+      }
+    });
+    this.setState({cities: tmp_cities});
+  }
+
+  get_API_KEY(){
+
+  }
+
+  //表示
+  render(){
+  }
 }
 
-export default App;
+export default ArtificialTrend;
