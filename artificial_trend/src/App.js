c30dda25e70b2cfd2c0715c23027358a783c3ece
tatsuomi <34530391+tatsuomi@users.noreply.github.com>
2022-01-15 21:35:14 +0900

    fixed format
    Use linters and formatters

--- artificial_trend/src/App.js
+++ artificial_trend/src/App.js
@@ -1,157 +1,162 @@
-import './App.css';
-import React from 'react';
-import { Component } from 'react/cjs/react.production.min';
-import Highcharts, { objectEach } from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
+import "./App.css";
+import React from "react";
+import { Component } from "react/cjs/react.production.min";
+import Highcharts, { objectEach } from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
-class ArtificialTrend extends Component{
+class ArtificialTrend extends Component {
   constructor(props) {
     super(props);
     this.state = {
       cities: [],
       series: [],
       categories: [],
-      city_flags: Array(47).fill(false)
+      city_flags: Array(47).fill(false),
     };
   }
 
   //都道府県取得
-  componentDidMount(){
+  componentDidMount() {
     //仮の都道府県配列
     var tmp_cities = [];
     fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures/", {
-      headers: {"X-API-KEY": this.get_API_KEY()}
+      headers: { "X-API-KEY": this.get_API_KEY() },
     })
-    .then(response => response.json())
-    .then(json => {
-      for(var i=0; i<json.result.length; i++){
-        tmp_cities.push(json.result[i].prefName)
-      }
-      this.setState({cities: tmp_cities}, ()=>{})
-    });
+      .then((response) => response.json())
+      .then((json) => {
+        for (var i = 0; i < json.result.length; i++) {
+          tmp_cities.push(json.result[i].prefName);
+        }
+        this.setState({ cities: tmp_cities }, () => {});
+      });
   }
 
   //APIキー取得
-  get_API_KEY(){
+  get_API_KEY() {
     
   }
 
   //チェックボックスの雛形
-  make_check_box(id, city){
-    return(
-      <div class="checkbox">
-        <input 
+  make_check_box(id, city) {
+    return (
+      <div id="checkbox">
+        <input
           type="checkbox"
           checked={this.state.city_flags[id]}
           onChange={() => this.toggle(id)}
         />
-          {city}
+        {city}
       </div>
     );
   }
 
   //反転
-  toggle(id){
+  toggle(id) {
     var flag_copy = this.state.city_flags.slice();
     //更新
     flag_copy[id] = !flag_copy[id];
     //追加
-    if(!this.state.city_flags[id]){
-      this.get_series(id)
+    if (!this.state.city_flags[id]) {
+      this.get_series(id);
     }
     //削除
-    else{
-      this.remove_series(id)
+    else {
+      this.remove_series(id);
     }
     //保存
     this.setState({
-      city_flags: flag_copy
-    })
+      city_flags: flag_copy,
+    });
   }
 
   //データ取得
-  get_series(id){
-    fetch("https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode="+String(id+1), {
-      headers: {"X-API-KEY": this.get_API_KEY()}
-    })
-    .then(response => response.json())
-    .then(json => {
-      //年度
-      var categories = [];
-      //人口
-      var num = [];
-      Object.keys(json.result.data[0].data).forEach(i => {
-        categories.push(json.result.data[0].data[i].year);
-        num.push(json.result.data[0].data[i].value);
-      });
-      const new_series = {
-        name: this.state.cities[id],
-        data: num,
-      };
-      //初回のみ更新
-      if(this.state.categories.length === 0){
-        this.setState({
-          categories: categories
-        })
+  get_series(id) {
+    fetch(
+      "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=" +
+        String(id + 1),
+      {
+        headers: { "X-API-KEY": this.get_API_KEY() },
       }
-      this.setState({
-        series: [...this.state.series, new_series]
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        //年度
+        var categories = [];
+        //人口
+        var num = [];
+        Object.keys(json.result.data[0].data).forEach((i) => {
+          categories.push(json.result.data[0].data[i].year);
+          num.push(json.result.data[0].data[i].value);
+        });
+        const new_series = {
+          name: this.state.cities[id],
+          data: num,
+        };
+        //初回のみ更新
+        if (this.state.categories.length === 0) {
+          this.setState({
+            categories: categories,
+          });
+        }
+        this.setState({
+          series: [...this.state.series, new_series],
+        });
       });
-    })
   }
 
   //データ削除
-  remove_series(id){
+  remove_series(id) {
     const series_copy = this.state.series.slice();
-    for(var i=0; i<series_copy.length; i++){
-      if(series_copy[i].name === this.state.cities[id]){
+    for (var i = 0; i < series_copy.length; i++) {
+      if (series_copy[i].name === this.state.cities[id]) {
         series_copy.splice(i, 1);
       }
     }
     this.setState({
-      series: series_copy
+      series: series_copy,
     });
   }
 
   //グラフの表示
-  make_graph(){
+  make_graph() {
     const options = {
       title: {
-        text: "人口推移"
+        text: "人口推移",
       },
       xAxis: {
         title: {
-          text: "年度"
+          text: "年度",
         },
-        categories: this.state.categories
+        categories: this.state.categories,
       },
       yAxis: {
         title: {
-          text: "人口数"
-        }
+          text: "人口数",
+        },
       },
-      series: this.state.series
-    }
-    return(
-      <HighchartsReact highcharts={Highcharts} options={options} />
-    )
+      plotOptions: {
+        series: {
+          label: {
+            connectorAllowed: false,
+          },
+        },
+      },
+      series: this.state.series,
+    };
+    return <HighchartsReact highcharts={Highcharts} options={options} />;
   }
 
   //表示
-  render(){
-    var list = this.state.cities;
-    
-    return(
+  render() {
+    return (
       <div>
         <div>都道府県</div>
-        <div class="checkbox_wrapper">
-          {list.map((city, i) => (this.make_check_box(i, city)))}
+        <div id="checkbox_wrapper">
+          {this.state.cities.map((city, i) => this.make_check_box(i, city))}
         </div>
-        <div>
-          {this.make_graph()}
-        </div>
+        <div>{this.make_graph()}</div>
       </div>
-    )
+    );
   }
 }
 
